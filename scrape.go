4ad129151af4d242b484d1c11d34cfd6fc96ed50
Abhishek5101 <miabhishekkulkarni@gmail.com>
2020-02-20 23:23:38 -0800

    scrapes amazon harry potter products

--- scrape.go
+++ scrape.go
@@ -2,58 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/gocolly/colly"
 )
 
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
 func main() {
-	// Instantiate default collector
 	c := colly.NewCollector()
-
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-	})
-
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
+	c.OnHTML("div.s-result-list.s-search-results.sg-row", func(e *colly.HTMLElement) {
+		e.ForEach("div.a-section.a-spacing-medium", func(_ int, e *colly.HTMLElement) {
+			var productName string
+
+			productName = e.ChildText("span.a-size-medium.a-color-base.a-text-normal")
+
+			if productName == "" {
+				return
+			}
+
+			fmt.Printf("Product Name: %s \n", productName)
+		})
 	})
 
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Visited", r.Request.URL)
-	})
-
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
-	})
-
-	c.OnHTML("tr td:nth-of-type(1)", func(e *colly.HTMLElement) {
-		fmt.Println("First column of a table row:", e.Text)
-	})
-
-	c.OnXML("//h1", func(e *colly.XMLElement) {
-		fmt.Println(e.Text)
-	})
-
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL)
-	})
+	c.Visit("https://www.amazon.com/s?k=harry+potter&ref=nb_sb_noss_1")
 }
