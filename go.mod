5fb82dda449390bd7713bab58d160c41dc2a1f64
Abhishek5101 <miabhishekkulkarni@gmail.com>
2020-02-24 22:14:59 -0800

    Echo added

--- go.mod
+++ go.mod
@@ -11,7 +11,13 @@ require (
 	github.com/gocolly/colly v1.2.0
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo/v4 v4.1.14
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d // indirect
+	golang.org/x/net v0.0.0-20200222125558-5a598a2470a0 // indirect
+	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
 	google.golang.org/appengine v1.6.5 // indirect
 )
--- scrape.go
+++ scrape.go
@@ -1,31 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
+	"os"
 	"github.com/gocolly/colly"
+	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
+type news struct {
+	Title []string `json: "title"`
+}
+
+func startServer(myData []string) {
+	e := echo.New()
+
+	e.GET("/", func(f echo.Context) error {
+		return f.JSON(http.StatusOK, myData)
+	})
+
+	fmt.Println("Server running: http://localhost:8000")
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
 func main() {
+	var myData [] string
+
+	// Instantiate default collector
 	c := colly.NewCollector()
 
+	// On every a element which has href attribute call callback
+	c.OnHTML("a.storylink", func(e *colly.HTMLElement) {
+		myData = append(myData, e.Text)
+		title := news{Title:myData}
+		byteArray, err := json.MarshalIndent(title, "", " ")
+		if err != nil{
+			fmt.Println(err)
+		}
+		file, err := os.Create("output.json")
+		if err != nil{
+			fmt.Print(err)
+		}
+		file.Write(byteArray)
+		defer file.Close()
+		fmt.Println(string(byteArray))
+	})
+
+	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.OnHTML("div.s-result-list.s-search-results.sg-row", func(e *colly.HTMLElement) {
-		e.ForEach("div.a-section.a-spacing-medium", func(_ int, e *colly.HTMLElement) {
-			var productName string
-
-			productName = e.ChildText("span.a-size-medium.a-color-base.a-text-normal")
-
-			if productName == "" {
-				return
-			}
-
-			fmt.Printf("Product Name: %s \n", productName)
-		})
-	})
-
-	c.Visit("https://www.amazon.com/s?k=harry+potter&ref=nb_sb_noss_1")
+	// Start scraping on https://news.ycombinator.com
+	c.Visit("https://news.ycombinator.com/")
+	startServer(myData)
 }
